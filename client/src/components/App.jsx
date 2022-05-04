f431d688cd8729aa2e173be5ffc7506bca090f55
Marilene Soares da Costa <lene626@gmail.com>
2022-05-03 19:11:46 -0600

    adds homepage

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import axios from 'axios';
 
-class App extends React.Component {
+const App = () => {
 
-  constructor() {
-    super();
-    this.state = {
-    };
-
-  }
-
-  componentDidMount() {
-
-  }
-
-
-  render() {
-    return (
-      <div className="main">
-        <p>hello world</p>
-      </div>);
-  }
+  return (
+    <div>
+      {/* banner */}
+      <div className="row banner">
+        <div className="col">
+          <h1> timePlanner</h1>
+        </div>
+        <div className="col">
+        </div>
+      </div>
+      {/* main */}
+      <div className="jumbotron jumbotron-fluid text-center">
+        <div className="row">
+          <h4 className="display-5"> CHART YOUR PLANS</h4>
+        </div>
+        <div className="row">
+          <div className="col"> </div>
+          <div className="col">
+            <button className="btn btn-info btn-md">My charts</button>
+          </div>
+          <div className="col"> </div>
+        </div>
+        <div className="row">
+          <div className="col"> </div>
+          <div className="col">
+            <button className="btn btn-info btn-md">New chart</button>
+          </div>
+          <div className="col"> </div>
+        </div>
+      </div>
+      <div className="row text-center">
+        <p className="footer">mvp</p>
+      </div>
+    </div >
+  )
 }
 
 export default App;
